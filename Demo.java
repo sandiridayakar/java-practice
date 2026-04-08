66ddaa68bfa6553a4510058342ddc4bb0921a837
sandiridayakar <sandiridayakar1@gamil.com>
2026-04-08 15:32:12 +0530

    firstcommit

---
+++ Demo.java
@@ -0,0 +1,20 @@
+abstract class car{
+    abstract void drive(); //abstract method declaration
+    public void playMusic(){
+        System.out.println("playing music"); //non abstract method
+    }
+
+}
+class Audi extends car{
+    public void drive(){
+        System.out.println("driving audi");
+    }
+}
+class Demo{
+    public static void main(String a[]){
+        car c = new Audi();
+        c.drive();
+        c.playMusic();
+
+    }
+}
